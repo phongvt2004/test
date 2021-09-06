7fdc5abc5c84553e3f671d15560e82bdb8e5a49d
phongvt2004 <58397401+phongvt2004@users.noreply.github.com>
2021-09-06 19:15:10 +0700

    Create index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
-const port = 80;
+const port = process.env.PORT;
+console.log(port);
 const server = require('http').Server(app);
 const io = require('socket.io')(server);
 server.listen(port)
